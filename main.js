32d570ed8819b4d73697a39bee372637b5c7fafa
Tania <taniie_bcn@hotmail.es>
2023-10-07 13:10:57 +0200

    added styles

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 //TRAER ELEMENTOS DEL HTML
 const btnSave = document.querySelector('.js-savedata'); //botón guardar datos
-const btnData = document.querySelector('.js-recoverdata'); //botón recuperar datos
+const btnRecover = document.querySelector('.js-recoverdata'); //botón recuperar datos
 const list = document.querySelector('.js-list'); //lista
 
 //ARRAY VACIO DATOS USUARIO
@@ -25,20 +25,22 @@ function getApiInfo () {
 }
 getApiInfo();
 
-//PINTA ESTRUCTURA USUARIOS
+//PINTA ESTRUCTURA 1 USUARIO
 function renderUser(user){
     let html="";
-    html+=`<li id="${user.id.value}" class="list-user js-list-user noFriend">
-        <h3 class="">${user.name.first}</h3>
-        <img class="img" src="${user.picture.large}"/>
-        <p>${user.location.city}</p>
-        <p>${user.login.username}</p>
+    html+=`<li id=" ${user.id.value}" class="list-user js-list-user noFriend">
+        <img class="img" src=" ${user.picture.large}"/>
+        <h3 class="name"> ${user.name.first}</h3>
+        <p class="city"> ${user.location.city}</p>
+        <p class="username"> ${user.login.username}</p>
+        
     </li>`;
     
      return html;
 
     
 }
+// BUCLE DE LOS 10 USUARIOS
 function renderList(listUser){
 list.innerHTML="";
 for (const item of listUser) {
@@ -47,7 +49,7 @@ for (const item of listUser) {
 addEventToUser();
 
 }
-
+//FUNCION PARA DECIR SI ES MI AMIGO
 function handleClickFriends(event){
     const idUserCliked = parseInt (event.currentTarget.id);
     console.log (event.currentTarget.id);
@@ -62,23 +64,24 @@ function handleClickFriends(event){
 
 function addEventToUser(){
     const allUser = document.querySelectorAll(".js-list-user");
-    //añadir la funcion a cada una de las series a las que haga click
+    //añadir la funcion a cada una de los usuarios a los que haga click
     for (const item of allUser) {
         item.addEventListener('click', handleClickFriends);
     }
     }
-    
+
+    //usuarios guardados
 function handleClickSave (event){
     event.preventDefault();
     localStorage.setItem('saveUser', JSON.stringify(listUser));
 }
-
+//usuarios recuperados
 function handleClickData(event){
     event.preventDefault();
     listUser = userLS;
     renderList (listUser);
 }
 
-
+//eventos botones
 btnSave.addEventListener('click', handleClickSave);
-btnData.addEventListener('click', handleClickData);
+btnRecover.addEventListener('click', handleClickData);
